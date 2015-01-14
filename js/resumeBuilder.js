46a59f5ac3a497e79dcb3180531f9093c2254cf1
Rita Morais <rita@tasboa.com>
2015-01-14 21:28:49 +0000

    Internationalize button
    Adds internationalized button

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -137,7 +137,17 @@ displayWork ();
 
 
 
+function inName(name) {
 
+  name = name.trim().split(" ");
+  console.log(name);
+  name[1] = name[1].toUpperCase();
+  name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+  return name[0] +" "+name[1];
+
+};
+$("#main").append(internationalizeButton);
 
 
 // function locationizer(work_object) {
